70eed8b12e41330016ef1a62443b8e356243b1d1
yangyusheng2018 <1611337277@qq.com>
2019-02-14 15:05:58 +0800

    提交文件

---
+++ themes/tmp-12/contact.php
@@ -0,0 +1,154 @@
+
+   <?php include('header.php'); ?>
+    <!-- Page Title -->
+    <h1 class="page-title fullwidth-padding">
+        <div class="container">
+           <a href="/"><?=$home1?> >> </a> <?=$contact1?>
+        </div>
+    </h1>
+
+
+    <!-- Map -->
+    <p class="map"><iframe width="100%" height="278" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=zh-CN&amp;geocode=&amp;q=%E4%B8%AD%E5%9B%BD%E4%B8%8A%E6%B5%B7%E6%B5%A6%E4%B8%9C%E6%96%B0%E5%8C%BA%E9%87%91%E6%A1%A5%E5%8D%97%E5%8C%BA%E5%BB%BA%E4%B8%9A%E8%B7%AF416%E5%8F%B7%E4%B8%AD%E5%9B%BD%E7%9F%BF%E6%9C%BA%E5%87%BA%E5%8F%A3%E7%94%9F%E4%BA%A7%E5%9F%BA%E5%9C%B0%E6%80%BB%E9%83%A8&amp;aq=0&amp;oq=%E4%B8%AD%E5%9B%BD%E7%9F%BF%E6%9C%BA&amp;sll=31.224693,121.615391&amp;sspn=0.105105,0.181789&amp;gl=US&amp;ie=UTF8&amp;hq=%E5%8D%97%E5%8C%BA%E5%BB%BA%E4%B8%9A%E8%B7%AF416%E5%8F%B7%E4%B8%AD%E5%9B%BD%E7%9F%BF%E6%9C%BA%E5%87%BA%E5%8F%A3%E7%94%9F%E4%BA%A7%E5%9F%BA%E5%9C%B0%E6%80%BB%E9%83%A8&amp;hnear=%E4%B8%AD%E5%9B%BD%E4%B8%8A%E6%B5%B7%E5%B8%82%E6%B5%A6%E4%B8%9C%E6%96%B0%E5%8C%BA%E9%87%91%E6%A1%A5%E9%95%87&amp;ll=31.210859,121.689482&amp;spn=0.02628,0.045447&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=5228558243481044880&amp;output=embed"></iframe></p>
+
+
+    <!-- Content
+    ======================================================================== -->
+    <div id="content" class="container">
+
+        <!-- Main Content -->
+        <div id="main">
+
+      
+            <h3><?=$contact1h1?></h3>
+ <?=$contact1h1p?></p>
+            <!-- Contact Form -->
+           
+<?=$message1?>
+<div class="one-third">
+            
+        </div>
+
+ <div class="one-third">
+            <h3>Malaysia Office:</h3>
+            <p>Add: Kuala Lumpur</p>
+            <p>Contact person: Bill Liu</p>
+            <p>Mobile: 0060-169785618 </p>
+            <p>E-mail: chat@indh.net </p>
+</div>
+          <div class="one-third">
+            <h3>Philippines Office:</h3>
+           <p> Add：Makati City, Manila</p>
+            <p>Contact Person: Owen</p>
+           <p> Mobile:0063-9369222066</p>
+           <p> E-mail: chat@indh.net </p>
+          </div>
+          <div class="one-third">
+            <h3 id="indonesia">Indonesia office address</h3>
+            <p>Add: MOI. Kelapa Gading, Jakarta Utara, 10640</p>
+            <p> Mobile: 0062-(0)81282087161 </p>
+             <p>Contact Person: Victor </p>
+           <p> E-mail: chat@indh.net </p>
+          </div>
+          <div class="one-third">
+            <h3 id="india">India office address</h3>
+           <p> Add: B-32, 4th floor, Ekta, Opp. fire brigade office. Near Oberoi mall. Goregaon (E). Mumbai-63 .              </p>
+          <p>  Tel: 0091 9928911119, 00919076003100 </p>
+           <p>  Contact Person: Vijay Kumar </p>
+           <p> E-mail: chat@indh.net </p>
+          </div>
+          <div class="one-third">
+            <h3 id="Russian">Russian office address</h3>
+            <p>Add: Russian Federation, 143441, Moscow region, Krasnogorsk district, Putilkovo Village, 69 km. MKAD, Social office complex  JSC "Greenwood", building 17    </p>
+            <p>Contact: Valya Chen +79647615135 +8613761994116</p>
+            <p>Roma Zhai +79162846261 +8618221396129 </p>
+           <p> E-mail: chat@indh.net </p>
+          </div>
+          <div class="one-third">
+            <h3>Saudi Arabia office address</h3> 
+           <p> Location: Jeddah</p>
+           <p> Contact people: Steven</p>
+           <p>Cell phone: 00966 533692399</p>
+           <p> Email: chat@indh.net</p>
+          </div>
+          <div class="one-third">
+          <h3>Kenya Branch</h3>
+         <p> Add: Upperhill area, Nairobi</p>
+         <p> Contact Person: Mr. Frank Ma</p>
+         <p> Mobile: +254 714552214, </p>
+         <p> Email: chat@indh.net</p>
+          </div>
+          
+
+        </div>
+
+        <!-- Sidebar -->
+        <div id="sidebar">
+        
+        
+        
+       
+            <div class="headOffice">
+             
+              <div class="headAdd">
+                <h3 id="head2">Head office address</h3>
+                <p>No.416 Jianye Road, South Jinqiao Area, Pudong, Shanghai, China</p>
+                 <p>Zip: 201201                  </p>
+                 <p>Tel: 0086-21-58386256 0086-21-58386258</p>
+                <p> Fax: 0086-21-58385887</p>
+                <p>E-mail: chat@indh.net </p>
+              </div>
+              <div class="clear"></div>
+            </div>
+            <br><br>
+          <div class="agencyOffice">
+            <h3 id="dubai">Dubai office address</h3>
+           <p> Add: No.802 office bldg, Emirates Concord Tower, Al Makhtoum, Deira, Dubai.</p>
+             <p> P.O.Box: 171810</p>
+           <p>  TEL/FAX: 00971-4-2368234</p>
+            <p> Tel: 00971-56-7156843 </p>
+            <p>Contact Person: Evan              </p>
+            <p>MSN: evansong2011@hotmail.com</p>
+            <p> E-mail: chat@indh.net </p>
+            </div>
+          <div class="agencyOffice">
+            <h3 id="Nigeria">Nigeria office address</h3>
+            <p>Add: Lagos, Nigeria.</p>
+            <p>Kerry:00234 8061290205</p>
+
+            <p>E-mail: chat@indh.net </p>
+          </div>
+          <div class="agencyOffice">
+            <h3 id="Vie">Vietnam office address</h3>
+            <p>Add: Hanoi</p>
+            <p>Contact Person: Forest </p>
+            <p>Tel: 0084-964390389 </p>
+            <p>E-mail: chat@indh.net </p>
+          </div>
+         
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+          
+            <hr class="sep30">
+            
+
+        </div>
+
+    </div>
+    <!-- Content / End -->
+
+
+    <!-- Footer
+    ======================================================================== -->
+       
+   <?php include('footer.php'); ?>
